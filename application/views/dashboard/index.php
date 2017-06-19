a4fd61c2d244c804832945f3c1ba0fee318becac
Dhika Anbiya <dhika.anbiya@gmail.com>
2017-06-20 03:19:53 +0700

    fixing dashboard template

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -3,7 +3,7 @@
 		  		<div class="col-md-6">
 		  			<div class="content-box-large">
 		  				<div class="panel-heading">
-							<div class="panel-title">New vs Returning Visitors</div>
+							<div class="panel-title">Welcome</div>
 							
 							<div class="panel-options">
 								<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
@@ -11,19 +11,20 @@
 							</div>
 						</div>
 		  				<div class="panel-body">
-		  					Ut tristique adipiscing mauris, sit amet suscipit metus porta quis. Donec dictum tincidunt erat, eu blandit ligula. Nam sit amet dolor sapien. Quisque velit erat, congue sed suscipit vel, feugiat sit amet enim. Suspendisse interdum enim at mi tempor commodo. Sed tincidunt sed tortor eu scelerisque. Donec luctus malesuada vulputate. Nunc vel auctor metus, vel adipiscing odio. Aliquam aliquet rhoncus libero, at varius nisi pulvinar nec. Aliquam erat volutpat. Donec ut neque mi. Praesent enim nisl, bibendum vitae ante et, placerat pharetra magna. Donec facilisis nisl turpis, eget facilisis turpis semper non. Maecenas luctus ligula tincidunt iasdsd vitae ante et, 
-				  			<br /><br />
-				  			Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque sed consectetur erat. Maecenas in elementum libero. Sed consequat pellentesque ultricies. Ut laoreet vehicula nisl sed placerat. Duis posuere lectus n, eros et hendrerit pellentesque, ante magna condimentum sapien, eget ultrices eros libero non orci. Etiam varius diam lectus.
-							<br /><br />
+		  					<?php $user = $this->ion_auth->user()->row(); ?>
+		  					<h3>Hello, <?php echo $user->username;?></h3>
+		  					This is your backend page to manage all the content on the frontend. Have yourself at home, for any question or issues don't mind to contact my email by <a href="mailto:dhika.anbiya@gmail.com?Subject=[dk-internusa]issues" target="_top"><h4><span class="label label-info"><i class="fa fa-envelope-o"> </i> clicking this</span></h4></a>
+							<br />
 		  				</div>
 		  			</div>
 		  		</div>
+
 
 		  		<div class="col-md-6">
 		  			<div class="row">
 		  				<div class="col-md-12">
 		  					<div class="content-box-header">
-			  					<div class="panel-title">New vs Returning Visitors</div>
+			  					<div class="panel-title">Update Password</div>
 								
 								<div class="panel-options">
 									<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
@@ -31,36 +32,29 @@
 								</div>
 				  			</div>
 				  			<div class="content-box-large box-with-header">
-				  				
-					  			Pellentesque luctus quam quis consequat vulputate. Sed sit amet diam ipsum. Praesent in pellentesque diam, sit amet dignissim erat. Aliquam erat volutpat. Aenean laoreet metus leo, laoreet feugiat enim suscipit quis. Praesent mauris mauris, ornare vitae tincidunt sed, hendrerit eget augue. Nam nec vestibulum nisi, eu dignissim nulla.
-								<br /><br />
+				  				<form method="post" action="<?php echo base_url();?>manage/update">
+				  				<div class="form-group">
+		  					    <label for="password">New Password</label>
+		  					  	<input type="password" class="form-control" name="password"></textarea>  
+		  					  </div>
+		  					  <div class="form-group">
+		  					    <label for="password">Confirm New Password</label>
+		  					  	<input type="password" class="form-control" name="password"></textarea>  
+		  					  </div>
+		  					  <div class="form-group">		  					    
+		  					  	<button class="btn btn-info" type="submit" name="submit">update</button>
+		  					  </div>				  					
+				  				</form>									
 							</div>
 		  				</div>
-		  			</div>
-		  			<div class="row">
-		  				<div class="col-md-12">
-		  					<div class="content-box-header">
-			  					<div class="panel-title">New vs Returning Visitors</div>
-								
-								<div class="panel-options">
-									<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
-									<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
-								</div>
-				  			</div>
-				  			<div class="content-box-large box-with-header">
-				  				
-					  			Pellentesque luctus quam quis consequat vulputate. Sed sit amet diam ipsum. Praesent in pellentesque diam, sit amet dignissim erat. Aliquam erat volutpat. Aenean laoreet metus leo, laoreet feugiat enim suscipit quis. Praesent mauris mauris, ornare vitae tincidunt sed, hendrerit eget augue. Nam nec vestibulum nisi, eu dignissim nulla.
-								<br /><br />
-							</div>
-		  				</div>
-		  			</div>
+		  			</div>		  			
 		  		</div>
 		  	</div>
 
 		  	<div class="row">
-		  		<div class="col-md-12 panel-warning">
+		  		<div class="col-md-6 panel-info">
 		  			<div class="content-box-header panel-heading">
-	  					<div class="panel-title ">New vs Returning Visitors</div>
+	  					<div class="panel-title ">Update Basic Information</div>
 						
 						<div class="panel-options">
 							<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
@@ -68,8 +62,43 @@
 						</div>
 		  			</div>
 		  			<div class="content-box-large box-with-header">
-			  			Pellentesque luctus quam quis consequat vulputate. Sed sit amet diam ipsum. Praesent in pellentesque diam, sit amet dignissim erat. Aliquam erat volutpat. Aenean laoreet metus leo, laoreet feugiat enim suscipit quis. Praesent mauris mauris, ornare vitae tincidunt sed, hendrerit eget augue. Nam nec vestibulum nisi, eu dignissim nulla.
-						<br /><br />
+			  			<form method="post" action="<?php echo base_url();?>manage/info">
+				  				<div class="form-group">
+		  					    <label for="first_name">First Name</label>
+		  					  	<input type="text" class="form-control" name="fname"></textarea>  
+		  					  </div>
+		  					  <div class="form-group">
+		  					    <label for="password">Last Name</label>
+		  					  	<input type="text" class="form-control" name="lname"></textarea>  
+		  					  </div>
+		  					  <div class="form-group">
+		  					    <label for="password">Email</label>
+		  					  	<input type="email" class="form-control" name="email"></textarea>  
+		  					  </div>
+		  					  <div class="form-group">		  					    
+		  					  	<button class="btn btn-info" type="submit" name="submit">update</button>
+		  					  </div>				  					
+				  				</form>						
+					</div>
+		  		</div>
+		  		<div class="col-md-6 panel-success">
+		  			<div class="content-box-header panel-heading">
+	  					<div class="panel-title ">User List :: Latest 5</div>
+						
+						<div class="panel-options">
+							<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+							<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+						</div>
+		  			</div>
+		  			<div class="content-box-large box-with-header">
+			  			<div class="row">
+			  				<div class="col-md-1 col-sm-1 col-xs-1">#1</div>
+			  				<div class="col-md-4 col-sm-4 col-xs-4">dhika.anbiya@gmail.com</div>
+			  				<div class="col-md-3 col-sm-3 col-xs-3"><span class="label label-info">active</span></div>
+			  				<div class="col-md-3 col-sm-3 col-xs-3"><span class="label label-danger">delete</span></div>
+			  				
+
+			  			</div>
 					</div>
 		  		</div>
 		  	</div>
